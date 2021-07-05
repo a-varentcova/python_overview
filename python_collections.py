1de8525701998f63ce4482e0bd5eeab3b708e6d6
anst-zw <86804302+anst-zw@users.noreply.github.com>
2021-07-05 23:37:26 +0200

    Create python_collections.py

---
+++ python_collections.py
@@ -0,0 +1,5 @@
+from collections import defaultdict
+
+# defaultdict allows easily to handle missing keys:
+# when one tries to access a missing key, defaultdict automatically 
+# creates this key and generates a default value for it.
