0947321cb6f59840681db97a20610abef47412d1
anst-zw <86804302+anst-zw@users.noreply.github.com>
2021-07-06 23:45:25 +0200

    create defaultdict

--- python_collections.py
+++ python_collections.py
@@ -3,3 +3,5 @@ from collections import defaultdict
 # defaultdict allows easily to handle missing keys:
 # when one tries to access a missing key, defaultdict automatically 
 # creates this key and generates a default value for it.
+
+name_counter = defaultdict(int)
