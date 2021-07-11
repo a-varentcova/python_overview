57bbf27b9033891277bc52063279b895b771449d
anst-zw <86804302+anst-zw@users.noreply.github.com>
2021-07-11 23:42:13 +0200

    add lambda as a factory function

--- python_collections.py
+++ python_collections.py
@@ -1,9 +1,26 @@
 from collections import defaultdict
 
-# defaultdict allows easily to handle missing keys:
-# when one tries to access a missing key, defaultdict automatically 
-# creates this key and generates a default value for it.
+# defaultdict allows easily to handle missing keys: when one tries to access a missing key,
+# defaultdict automatically generates a default value for it.
 
 name_list = ["John", "Max", "James", "Peter", "Robert", "John", "John", "James"]
 
+# defaultdict takes a factory function as an argument which defines a default value.
+# Passing class int will generate 0 as the default value:
 name_counter = defaultdict(int)
+
+for name in name_list:
+    name_counter[name] += 1
+
+print(name_counter)
+
+# Also lambda functions can be used to change the default value.
+# For instance, to have 10 as default:
+name_counter = defaultdict(lambda: 10)
+
+for name in name_list:
+    name_counter[name] += 1
+
+print(name_counter)
+
+  
