f6b1442616161390a6d1344855d2a0496f54ec12
anst-zw <86804302+anst-zw@users.noreply.github.com>
2021-07-24 23:19:16 +0200

    add escape sequence examples

--- python_strings.py
+++ python_strings.py
@@ -16,6 +16,14 @@ def main():
     # converting to a string type before concatenation:
     print(full_name + ': ' + str(25))
     print(type(str(25)))
+    
+    # Escape sequence: any character after \ is treated as a string
+    # \t = tab
+    # \n = new line
+    print("it's a string with special characters")
+    print("it's a string with \"special\" characters")
+    print("\t it's a string with special characters")
+    print("it's a string with special characters\n and here is a new line")
 
 
 if __name__ == "__main__":
