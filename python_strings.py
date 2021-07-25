12b99b83db579826b1d7c5664e411d66e01e120f
anst-zw <86804302+anst-zw@users.noreply.github.com>
2021-07-25 19:41:23 +0200

    add strings formatting

--- python_strings.py
+++ python_strings.py
@@ -14,8 +14,9 @@ def main():
     print(full_name)
     
     # converting to a string type before concatenation:
-    print(full_name + ': ' + str(25))
-    print(type(str(25)))
+    age = 25
+    print(full_name + ': ' + str(age))
+    print(type(str(age)))
     
     # Escape sequence: any character after \ is treated as a string
     # \t = tab
@@ -25,6 +26,15 @@ def main():
     print("\t it's a string with special characters")
     print("it's a string with special characters\n and here is a new line")
 
+    # formatted strings
+    # f-strings (python3):
+    print(f"{first_name} {last_name} is {age} years old.")    # no need for explicit type conversion
+
+    # .format() method (python2):
+    print("{} {} is {} years old".format(first_name, last_name, last_name))
+    print("{1} {0} is {2} years old".format(last_name, first_name, last_name)) 
+    print("{first_name} is {age} years old".format(first_name="Arni", age=20))
+    
 
 if __name__ == "__main__":
     main()
