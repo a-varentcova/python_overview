f35163440cfd037e8791dd095bccd1b8f0561286
anst-zw <varentcova.a@gmail.com>
2021-08-14 21:56:16 +0200

    add custom generator and custom for-loop examples

--- python_generators.py
+++ python_generators.py
@@ -15,9 +15,59 @@ def generator_function(N):
 for i in generator_function(10):
     print(i)
 
+
 # how next works in generators:
 # yield pauses the functions, and next invokes it again by 1 iteration
-g = generator_function(10)
+g = generator_function(5)
 print(next(g))
 print(next(g))
 print(next(g))
+print(next(g))
+print(next(g))
+
+# exceeding number of items gives StopIteration error:
+# print(next(g))
+
+
+# creating custom for-loop:
+
+def new_for_loop(iterable):
+    # making a generator from any kind of iterable:
+    generator = iter(iterable)
+    while True:
+        try:
+            print(next(generator))
+        except StopIteration:
+            break
+
+g = generator_function(5)
+new_for_loop(g)
+
+g = [1,2,3,4,5]
+new_for_loop(g)
+
+
+# creating custom range object:
+
+class MyRange():
+    
+    def __init__(self, start, stop):
+        self.current = start
+        self.start = start
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.current < self.stop:
+            num = self.current
+            self.current += 1
+            return num
+        raise StopIteration
+
+
+new_range = MyRange(0,10)
+
+for i in new_range:
+    print(i)
