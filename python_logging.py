bb3640d982b65f612db334da2cf598fbfd6df971
anst-zw <varentcova.a@gmail.com>
2021-09-30 23:35:55 +0200

    add format to the basicConfig and the logfile

--- python_logging.py
+++ python_logging.py
@@ -2,8 +2,16 @@ import logging
 
 def main():
 
-    loggin.basicConfig(level=logging.DEBUG)
+    FORMAT = '%(asctime)s:%(levelname)s - %(message)s'
 
+    logging.basicConfig(
+        filename = 'progress.log',
+        level=logging.DEBUG,       # everythong above this level will be logged
+        format=FORMAT,             # formats the log message
+        filemode='w',              # rewrite every time as the program runs
+        )
+
+    # five default levels of logs:
     logging.debug("This is a debug message")
     logging.info("This is a info message")  
     logging.warning("This is a warning message")  
@@ -11,5 +19,5 @@ def main():
     logging.critical("This is a critical message")  
 
   
- if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
